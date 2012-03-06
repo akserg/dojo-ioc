42e422b09d502470eff220cbb9565e72c3d7c8c6
Sergey Akopkokhyants <akserg@gmail.com>
2012-03-06 11:11:09 +0200

    Fixed IE problems

--- test/UtilTestCase.js
+++ test/UtilTestCase.js
@@ -3,9 +3,6 @@ define([
 	"ioc/Util"
 ], function(doh, Util){
 	doh.register("UtilTestCase",[
-         // Util.isArray
-         function Util_isArrayTest(){ doh.assertTrue(Util.isArray([1, 2])); },
-         function Util_isArrayTest2(){ doh.assertFalse(Util.isArray(1, 2)); },
          // Util.inArray
          function Util_inArrayTest(){ doh.assertTrue(Util.inArray(1, [1, 2]) === 0); },
          function Util_inArrayTest2(){ doh.assertFalse(Util.inArray(3, [1, 2]) !== -1); },
